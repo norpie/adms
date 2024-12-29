974b0e1407255ccb32553da4825a73e09d8f58f3
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-12-29 14:49:26 +0100

    feat: Read-Field, gets the FieldName for logging

--- src/ad/util.ps1
+++ src/ad/util.ps1
@@ -2,21 +2,22 @@ Function Read-Field
 {
     param (
         $Field,
+        $FieldName,
         $Default
     )
     if (-not $Field)
     {
         if ($Default)
         {
-            Write-Log-Abstract -Category 'INF' -MessageName 'MissingFieldDefault' -AdditionalMessage "$Field"
+            Write-Log-Abstract -Category 'INF' -MessageName 'MissingFieldDefault' -AdditionalMessage "$(FieldName): $Default"
             return $Default
         }
         if ($global:ADOptions.ErrorHandling -eq 3)
         {
-            Write-Log-Abstract -Category 'ERR' -MessageName 'MissingField' -AdditionalMessage "$Field" -Throw
+            Write-Log-Abstract -Category 'ERR' -MessageName 'MissingField' -AdditionalMessage "$FieldName" -Throw
         } elseif ($global:ADOptions.ErrorHandling -eq 2)
         {
-            Write-Log-Abstract -Category 'WAR' -MessageName 'MissingField' -AdditionalMessage "$Field"
+            Write-Log-Abstract -Category 'WAR' -MessageName 'MissingField' -AdditionalMessage "$FieldName"
         }
     }
     return $Field
