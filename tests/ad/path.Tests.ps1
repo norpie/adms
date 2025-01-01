94b14effd4e51ef6b734ddf34dfd53cc4471578b
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-01 14:36:31 +0100

    tests: added more tests for path parsing

--- tests/ad/path.Tests.ps1
+++ tests/ad/path.Tests.ps1
@@ -1,10 +1,19 @@
 
 
-Describe "Get-Parsed-Path" {
+Describe "Paths" {
     BeforeAll {
         . $PSCommandPath.Replace('tests', 'src').Replace('.Tests.ps1','.ps1')
     }
-    It "Should return a parsed path" {
-        Get-Parsed-Path -Path "Example/Path/To/OU" -TopLevel "DC=Example,DC=com" | Should -Be "OU=OU,OU=To,OU=Path,OU=Example,DC=Example,DC=com"
+    It "Parsing a path" {
+        Get-Parsed-Path -Path "ROOT/Example/Path/To/OU" -TopLevel "DC=Example,DC=com" | Should -Be "OU=OU,OU=To,OU=Path,OU=Example,DC=Example,DC=com"
+    }
+    It "Parsing an empty path" {
+        Get-Parsed-Path -Path "" -TopLevel "DC=Example,DC=com" | Should -Be "DC=Example,DC=com"
+    }
+    It "Unparsing a path" {
+        Get-Unparsed-Path -Path "OU=OU,OU=To,OU=Path,OU=Example,DC=Example,DC=com" -TopLevel "DC=Example,DC=com" | Should -Be "ROOT/Example/Path/To/OU"
+    }
+    It "Unparsing a top level path" {
+        Get-Unparsed-Path -Path "DC=Example,DC=com" -TopLevel "DC=Example,DC=com" | Should -Be "ROOT"
     }
 }
