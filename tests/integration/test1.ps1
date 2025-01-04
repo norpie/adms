c70654ed7b5ed014fb6c8a77bd203707c9e7d1d0
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-04 23:22:31 +0100

    tests: integration test does all actions

--- tests/integration/test1.ps1
+++ tests/integration/test1.ps1
@@ -1,7 +1,22 @@
 $ProjectDir = $PSCommandPath.replace('tests\integration\test1.ps1', '')
+$Script = "$ProjectDir\src\main.ps1"
 
-& "$ProjectDir\src\main.ps1" -Reset
-& "$ProjectDir\src\main.ps1" -Entity OU -File .\example\add_action_ous.csv -MakeReport
-& "$ProjectDir\src\main.ps1" -Entity User -File .\example\add_action_users.csv -MakeReport
-& "$ProjectDir\src\main.ps1" -Entity Group -File .\example\add_action_groups.csv -MakeReport
-& "$ProjectDir\src\main.ps1" -Entity GroupUser -File .\example\add_action_users_for_groups.csv -MakeReport
+& $Script -Reset -ConsoleVerbosity 2
+
+# Add actions
+& $Script -Entity OU -File .\example\add_action_ous.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity User -File .\example\add_action_users.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity Group -File .\example\add_action_groups.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity GroupUser -File .\example\add_action_users_for_groups.csv -MakeReport -ConsoleVerbosity 2
+
+# Modify actions
+& $Script -Entity Group -File .\example\modify_action_groups.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity User -File .\example\modify_action_users.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity OU -File .\example\modify_action_ous.csv -MakeReport -ConsoleVerbosity 2
+
+# Remove actions
+& $Script -Entity GroupUser -File .\example\remove_action_users_groups.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity Group -File .\example\remove_action_groups.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity User -File .\example\remove_action_users.csv -MakeReport -ConsoleVerbosity 2
+# Use recursive here because I am too lazy to invert the file's lines
+& $Script -Entity OU -File .\example\remove_action_ous.csv -MakeReport -RecursiveDelete -ConsoleVerbosity 2
