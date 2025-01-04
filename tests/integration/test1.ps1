d613a6ef25ba63f8a04f5ed5048ae1956c6f1e23
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-04 16:29:27 +0100

    test: remove unnecessary flags from integration testing

--- tests/integration/test1.ps1
+++ tests/integration/test1.ps1
@@ -1,7 +1,7 @@
 $ProjectDir = $PSCommandPath.replace('tests\integration\test1.ps1', '')
 
 & "$ProjectDir\src\main.ps1" -Reset
-& "$ProjectDir\src\main.ps1" -Entity OU -File .\example\action_ous.csv -RecursiveDelete -MakeReport -OverwriteExisting
-& "$ProjectDir\src\main.ps1" -Entity User -File .\example\action_users.csv -RecursiveDelete -MakeReport -OverwriteExisting
-& "$ProjectDir\src\main.ps1" -Entity Group -File .\example\action_groups.csv -RecursiveDelete -MakeReport -OverwriteExisting
-& "$ProjectDir\src\main.ps1" -Entity GroupUser -File .\example\action_users_for_groups.csv -RecursiveDelete -MakeReport -OverwriteExisting
+& "$ProjectDir\src\main.ps1" -Entity OU -File .\example\add_action_ous.csv -MakeReport
+& "$ProjectDir\src\main.ps1" -Entity User -File .\example\add_action_users.csv -MakeReport
+& "$ProjectDir\src\main.ps1" -Entity Group -File .\example\add_action_groups.csv -MakeReport
+& "$ProjectDir\src\main.ps1" -Entity GroupUser -File .\example\add_action_users_for_groups.csv -MakeReport
