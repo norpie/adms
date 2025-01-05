c55ab1456c84cb612cff92d6c992d6034719cd79
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-05 12:03:18 +0100

    tests: ou tests more indent

--- tests/ad/ou.Tests.ps1
+++ tests/ad/ou.Tests.ps1
@@ -1,73 +1,75 @@
-Describe "OU Invoking" {
+Describe "OU" {
 
     BeforeAll {
         . $PSCommandPath.Replace('tests', 'src').Replace('.Tests.ps1','.ps1')
     }
 
-    Context "When action is 'Add'" {
-        BeforeEach {
-            $Action = @{
-                Action = "Add"
-                Path = "ROOT"
-                Name = "Company"
-                Protected = $true
-                Description = "Top-level company organisational unit"
+    Context "Invoke-OU-Action" {
+        Context "When action is 'Add'" {
+            BeforeEach {
+                $Action = @{
+                    Action = "Add"
+                    Path = "ROOT"
+                    Name = "Company"
+                    Protected = $true
+                    Description = "Top-level company organisational unit"
+                }
+                Mock New-OU {} -Verifiable -ParameterFilter { $OU -eq $Action }
+
+                Invoke-OU-Action -Action $Action
             }
-            Mock New-OU {} -Verifiable -ParameterFilter { $OU -eq $Action }
 
-            Invoke-OU-Action -Action $Action
-        }
-
-        It "calls New-OU" {
-            Should -InvokeVerifiable
-        }
-    }
-
-    Context "When action is 'Modify'" {
-        BeforeEach {
-            $Action = @{
-                Action = "Modify"
-                Path = "ROOT"
-                Name = "Company"
-                Protected = $true
-                Description = "Top-level company organisational unit"
+            It "calls New-OU" {
+                Should -InvokeVerifiable
             }
-            Mock Set-OU {} -Verifiable -ParameterFilter { $OU -eq $Action }
-            Invoke-OU-Action -Action $Action
         }
 
-        It "calls Set-OU" {
-            Should -InvokeVerifiable
-        }
-    }
-
-    Context "When action is 'Remove'" {
-        BeforeEach {
-            $Action = @{
-                Action = "Remove"
-                Path = "ROOT"
-                Name = "Company"
+        Context "When action is 'Modify'" {
+            BeforeEach {
+                $Action = @{
+                    Action = "Modify"
+                    Path = "ROOT"
+                    Name = "Company"
+                    Protected = $true
+                    Description = "Top-level company organisational unit"
+                }
+                Mock Set-OU {} -Verifiable -ParameterFilter { $OU -eq $Action }
+                Invoke-OU-Action -Action $Action
             }
-            Mock Remove-OU {} -Verifiable -ParameterFilter { $OU -eq $Action }
-            Invoke-OU-Action -Action $Action
-        }
 
-        It "calls Remove-OU" {
-            Should -InvokeVerifiable
-        }
-    }
-
-    Context "When action is not applicable" {
-        BeforeAll {
-            $Action = @{
-                Action = "Invalid"
+            It "calls Set-OU" {
+                Should -InvokeVerifiable
             }
-            Mock Write-Log-Abstract {} -Verifiable -ParameterFilter { $Throw }
-            Invoke-OU-Action -Action $Action
         }
 
-        It "Writes log with throw" {
-            Should -InvokeVerifiable
+        Context "When action is 'Remove'" {
+            BeforeEach {
+                $Action = @{
+                    Action = "Remove"
+                    Path = "ROOT"
+                    Name = "Company"
+                }
+                Mock Remove-OU {} -Verifiable -ParameterFilter { $OU -eq $Action }
+                Invoke-OU-Action -Action $Action
+            }
+
+            It "calls Remove-OU" {
+                Should -InvokeVerifiable
+            }
+        }
+
+        Context "When action is not applicable" {
+            BeforeAll {
+                $Action = @{
+                    Action = "Invalid"
+                }
+                Mock Write-Log-Abstract {} -Verifiable -ParameterFilter { $Throw }
+                Invoke-OU-Action -Action $Action
+            }
+
+            It "Writes log with throw" {
+                Should -InvokeVerifiable
+            }
         }
     }
 }
