cbf0d520ca01cc54a67878b5b6691638f9eaa564
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-05 09:48:10 +0100

    tests: integration test console verbosity param

--- tests/integration/test1.ps1
+++ tests/integration/test1.ps1
@@ -1,22 +1,26 @@
+param (
+    $ConsoleVerbosity = 2
+)
+
 $ProjectDir = $PSCommandPath.replace('tests\integration\test1.ps1', '')
 $Script = "$ProjectDir\src\main.ps1"
 
-& $Script -Reset -ConsoleVerbosity 2
+& $Script -Reset -ConsoleVerbosity $ConsoleVerbosity
 
 # Add actions
-& $Script -Entity OU -File .\example\add_action_ous.csv -MakeReport -ConsoleVerbosity 2
-& $Script -Entity User -File .\example\add_action_users.csv -MakeReport -ConsoleVerbosity 2
-& $Script -Entity Group -File .\example\add_action_groups.csv -MakeReport -ConsoleVerbosity 2
-& $Script -Entity GroupUser -File .\example\add_action_users_for_groups.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity OU -File .\example\add_action_ous.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
+& $Script -Entity User -File .\example\add_action_users.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
+& $Script -Entity Group -File .\example\add_action_groups.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
+& $Script -Entity GroupUser -File .\example\add_action_users_for_groups.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
 
 # Modify actions
-& $Script -Entity Group -File .\example\modify_action_groups.csv -MakeReport -ConsoleVerbosity 2
-& $Script -Entity User -File .\example\modify_action_users.csv -MakeReport -ConsoleVerbosity 2
-& $Script -Entity OU -File .\example\modify_action_ous.csv -MakeReport -ConsoleVerbosity 2
+& $Script -Entity Group -File .\example\modify_action_groups.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
+& $Script -Entity User -File .\example\modify_action_users.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
+& $Script -Entity OU -File .\example\modify_action_ous.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
 
 # Remove actions
-& $Script -Entity GroupUser -File .\example\remove_action_users_groups.csv -MakeReport -ConsoleVerbosity 2
-& $Script -Entity Group -File .\example\remove_action_groups.csv -MakeReport -ConsoleVerbosity 2
-& $Script -Entity User -File .\example\remove_action_users.csv -MakeReport -ConsoleVerbosity 2
-# Use recursive here because I am too lazy to invert the file's lines
-& $Script -Entity OU -File .\example\remove_action_ous.csv -MakeReport -RecursiveDelete -ConsoleVerbosity 2
+& $Script -Entity GroupUser -File .\example\remove_action_users_groups.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
+& $Script -Entity Group -File .\example\remove_action_groups.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
+& $Script -Entity User -File .\example\remove_action_users.csv -MakeReport -ConsoleVerbosity $ConsoleVerbosity
+# Use recursive here because I am too lazy to match the removes above, otherwise we error with: "The operation cannot be performed because child objects..."
+& $Script -Entity OU -File .\example\remove_action_ous.csv -MakeReport -RecursiveDelete -ConsoleVerbosity $ConsoleVerbosity
